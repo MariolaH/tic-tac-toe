43f11316f01075db3453eb61724bb66e1ec26077
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-02-23 04:13:36 +0000

    added reset button

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,28 @@
 let app = document.getElementById('app');
 
+let frame = document.createElement('frame');
+frame.classList.add('frame');
+app.appendChild(frame);
+
+// let frame = document.createElement('div');
+// frame.className = 'text-center'
+
+// Player 1
+let player1 = document.createElement('player1');
+player1.setAttribute('class', "frame");
+player1.textContent = 'P L A Y E R: 1'
+frame.appendChild(player1);
+
+// Player 2
+
+let player2 = document.createElement('player2');
+player2.setAttribute('class', "frame");
+player2.textContent = ('P L A Y E R: 2')
+frame.appendChild(player2);
+
 // Created title for the game 
 
-let header = document.createElement('TIC-TAC-TOE')
+let header = document.createElement('TIC-TAC-TOE');
 header.setAttribute('class', "header");
 header.textContent = 'TIC-TAC-TOE'
 app.appendChild(header);
@@ -36,6 +56,8 @@ function clickTile(e) {
     e.target.disabled = true;
 }
 
+
+
 // created tiles on board
 
 function buildBoard() {
@@ -49,7 +71,37 @@ function buildBoard() {
 
 buildBoard();
 
-// reset 
+let button = document.createElement('button')
+button.textContent = 'R E S E T'
+app.appendChild(button);
+
+// winConditions
+// 		[0,1,2]
+// 		[3,4,5]
+// 		[6,7,8]
+// 	    [0,3,6]
+// 		[1,4,7]
+// 		[2,5,8]
+// 		[0,4,8]
+// 		[2,4,6]
+
+// reset button
+
+// function to reset game
+
+// function reset() {
+
+//     if clickTile === winCondition
+//         then winner
+//         else clickTile != winCondition
+//         then draw
+// } 
+
+
+// need to connect clicktile to who's turn it is
+
+
+
 
 
 
